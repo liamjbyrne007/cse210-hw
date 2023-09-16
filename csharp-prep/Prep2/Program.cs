55c0a6e1ff369a6843b9f54824b665f314c07528
liamjbyrne007 <liamjbyrne007@gmail.com>
2023-09-16 17:55:14 -0600

    Completed prep 1 and prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What was your grade percentage? ");
+        string grade = Console.ReadLine();
+        int percent = int.Parse(grade);
+
+        string letterGrade = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent < 90 && percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent < 80 && percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent < 70 && percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+        Console.WriteLine($"Your grade is {letterGrade}");
+        if (letterGrade == "A" || letterGrade == "B" || letterGrade == "C")
+        {
+            Console.WriteLine("Good job, you have passed.");
+        }
+        else if (letterGrade == "D" || letterGrade == "F")
+        {
+            Console.WriteLine("You failed.");
+        }
     }
 }
