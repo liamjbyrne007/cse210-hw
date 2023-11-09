ec3b5af7ce375787e332bb6d4cde32c80adfede1
liamjbyrne007 <liamjbyrne007@gmail.com>
2023-11-09 16:06:41 -0700

    Finished Learning 05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square s = new Square("Green", 7);
+        shapeList.Add(s);
+
+        Rectangle r = new Rectangle("Purple", 5, 4);
+        shapeList.Add(r);
+
+        Circle c = new Circle("Blue", 3);
+        shapeList.Add(c);
+
+        foreach (Shape shape in shapeList)
+        {
+            Console.WriteLine(shape.GetColor());
+            Console.WriteLine(shape.GetArea());
+        } 
     }
 }
